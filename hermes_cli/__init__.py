d898b5c5b2433e62731c29dc929654c8906bb4bd
Rohit Sharma <interstellar.consulting@gmail.com>
2026-06-29 20:02:40 +0200

    chore(release): bump version to 0.17.2

--- hermes_cli/__init__.py
+++ hermes_cli/__init__.py
@@ -14,8 +14,8 @@ Provides subcommands for:
 import os
 import sys
 
-__version__ = "0.17.1"
-__release_date__ = "2026.6.26"
+__version__ = "0.17.2"
+__release_date__ = "2026.6.29"
 
 
 def _ensure_utf8():
