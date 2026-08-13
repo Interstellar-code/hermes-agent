e6a344f5389f8f7f9be5952c1b1e79f361796021
Interstellar-code <rohit.sharma.g@gmail.com>
2026-08-13 06:24:05 +0530

    chore: bump version to v0.19.14 (2026.8.13)

--- hermes_cli/__init__.py
+++ hermes_cli/__init__.py
@@ -14,8 +14,8 @@ Provides subcommands for:
 import os
 import sys
 
-__version__ = "0.19.13"
-__release_date__ = "2026.8.12.2"
+__version__ = "0.19.14"
+__release_date__ = "2026.8.13"
 
 
 def _ensure_utf8():
